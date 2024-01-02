adb2185096125754411a78b4720010a8fb7a4860
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2024-01-02 21:15:36 +0200

    installation Tailwind and ui.shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+
 import { Route, Routes } from "react-router-dom"
 import { SigninForm } from "./auth/forms/SigninForm"
 import SignupForm from "./auth/forms/SignupForm"
-import { Home } from "./pages"
+import { Home } from "./root/pages"
 import { AuthLayout } from "./auth/AuthLayout"
+import {RootLayout} from './root/RootLayout'
 
 
 function App() {
@@ -10,7 +12,9 @@ function App() {
   return (
 <>
     <main className="flex h-screen">
-
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     <Routes>
       {/* {'public route'} */}
       <Route element={<AuthLayout />}>
@@ -18,8 +22,11 @@ function App() {
           <Route path="/sign-up" element={<SignupForm />}/>
       </Route>
 
+
       {/* {private route} */}
-      <Route index element={<Home/>} /> 
+      <Route element={<RootLayout/>}>
+          <Route index element={<Home/>} /> 
+      </Route>
     </Routes>
     </main>
 </>
