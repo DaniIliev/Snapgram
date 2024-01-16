8fc329f52bde559e2a2c76fe56793060932887da
DaniIliev <danieljivkov04@abv.bg>
2024-01-16 15:04:48 +0200

    add input fields

--- src/auth/forms/SignupForm.tsx
+++ src/auth/forms/SignupForm.tsx
@@ -34,28 +34,72 @@ const SignupForm = () => {
   return (
     <>
       <Form {...form}>
-        <div className="sm:w-420 flex-center flex-col">
+        <div className="flex flex-col flex-center items-center sm:w-420">
           <img src="/logo.svg" alt="logo" />
+          <h2 className="text-white h3-bold md:h2-bold pt-5 sm:pt-10">Create a new account</h2>
+          <p className="text-slate-300 small-medium md:base-regular mt-2">To use use Snapgram enter your account details</p>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="white">Name</FormLabel>
+                  <FormControl>
+                    <Input type="text" className=" bg-slate-600 text-white border-slate-50" />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="white">Username</FormLabel>
+                  <FormControl>
+                    <Input type="text" className=" bg-slate-600 text-white border-slate-50" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="white">Email</FormLabel>
+                  <FormControl>
+                    <Input type="text" className=" bg-slate-600 text-white border-slate-50" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="white">Password</FormLabel>
+                  <FormControl>
+                    <Input type="text" className=" bg-slate-600 text-white border-slate-50" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+
+            <Button type="submit" className="bg-indigo-300 hover:bg-violet-500">Submit</Button>
+          </form>
         </div>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
       </Form>
     </>
   )
