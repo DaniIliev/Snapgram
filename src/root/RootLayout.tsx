069d2ec49f4398f7fdf1a30af79e1c8779375723
DaniIliev <danieljivkov04@abv.bg>
2024-01-18 12:33:50 +0200

    update RootLayout

--- src/root/RootLayout.tsx
+++ src/root/RootLayout.tsx
@@ -1,7 +1,18 @@
+import { BotomBar } from "@/components/shared/BotomBar"
+import LeftSideBar from "@/components/shared/LeftSideBar"
+import TopBar from "@/components/shared/TopBar"
 
+import { Outlet } from "react-router-dom"
 
 export const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className="w-full md:flex">
+      <TopBar />
+      <LeftSideBar />
+      <section className="flex flex-1 h-full">
+          <Outlet/>
+      </section>
+      <BotomBar />
+    </div>
   )
 }
