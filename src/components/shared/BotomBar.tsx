95aa5a85e44692ce5d02a765e5df5182e915e96a
DaniIliev <danieljivkov04@abv.bg>
2024-01-18 14:56:59 +0200

    create a botomBar for mobile devices

---
+++ src/components/shared/BotomBar.tsx
@@ -0,0 +1,31 @@
+import { bottombarLinks } from '@/constants'
+import { INavLink } from '@/types'
+import { Link, useLocation } from 'react-router-dom'
+
+export const BotomBar = () => {
+
+  const { pathname } = useLocation()
+  return (
+    <section className=' z-50 flex justify-between items-center w-full sticky bottom-0 rounded-t-[20px] bg-dark-2 px-5 py-4 md:hidden'>
+      {bottombarLinks.map((link: INavLink) => {
+        const isActive = pathname === link.route;
+        return (
+            <Link
+              to={link.route}
+              className={`flex flex-col justify-center items-center p-2 gap-1 transition text-white ${isActive &&
+                'bg-violet-300 border-ra rounded-[10px]'}`}
+            >
+              <img src={link.imgURL}
+                alt={link.label}
+                width={18}
+                height={18}
+              // className=' group-hover:invert brightness-0 transition'
+              />
+             <p className='tiny-medium text-white'>{link.label}</p> 
+            </Link>
+
+        )
+      })}
+    </section>
+  )
+}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -25,3 +25,26 @@ export const sidebarLinks = [
         label: "Create Post",
       },
 ]
+
+export const bottombarLinks = [
+  {
+    imgURL: "/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/icons/wallpaper.svg",
+    route: "/explore",
+    label: "Explore",
+  },
+  {
+    imgURL: "/icons/bookmark.svg",
+    route: "/saved",
+    label: "Saved",
+  },
+  {
+    imgURL: "/icons/gallery-add.svg",
+    route: "/create-post",
+    label: "Create",
+  },
+];
