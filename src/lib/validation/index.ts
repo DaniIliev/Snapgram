2a662462a41746c29990c309b492ab0b759616b2
DaniIliev <danieljivkov04@abv.bg>
2024-01-19 20:45:31 +0200

    add create post validation

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -13,3 +13,10 @@ export const singUpValidationSchema = z.object({
     password: z.string().min(8, {message: 'Password muss be at least 8 characters'})
   })
 
+  export const postValidationSchema = z.object({
+    caption: z.string().min(5).max(2200),
+    file: z.custom<File[]>(),
+    location: z.string().min(2).max(100),
+    tags: z.string()
+  })
+
