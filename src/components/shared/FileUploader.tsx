ea6cad22dc82c0d4eed01e5cd39c6f4c0c6661b7
DaniIliev <danieljivkov04@abv.bg>
2024-01-19 14:47:30 +0200

    show fileUploader

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,15 +1,15 @@
-import React, {useCallback, useState} from 'react'
-import {FileWithPath, useDropzone} from 'react-dropzone'
+import React, { useCallback, useState } from 'react'
+import { FileWithPath, useDropzone } from 'react-dropzone'
 import { Button } from '../ui/button'
 
-type FileUploaderProps ={
+type FileUploaderProps = {
     fieldChange: (FILES: File[]) => void;
     mediaUrl: string,
 }
 
-const FileUploader = ({fieldChange, mediaUrl}: FileUploaderProps) => {
-    const[file,setFile] = useState<File[]>([])
-    const[fileUrl,setFileUrl] = useState('')
+const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
+    const [file, setFile] = useState<File[]>([])
+    const [fileUrl, setFileUrl] = useState('')
 
     const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
         setFile(acceptedFiles)
@@ -18,33 +18,40 @@ const FileUploader = ({fieldChange, mediaUrl}: FileUploaderProps) => {
     }, [file])
 
     const { getRootProps, getInputProps } = useDropzone({
-         onDrop,
-         accept: {
+        onDrop,
+        accept: {
             'image/*': ['.png', 'jpeg', '.jpg', '.svg']
-         } 
-        })
+        }
+    })
     return (
         <div {...getRootProps()} className='flex justify-center items-center flex-col  bg-slate-600 rounded-xl cursor-pointer'>
-            <input {...getInputProps()} className='cursor-pointer'/>
+            <input {...getInputProps()} className='cursor-pointer' />
             {
                 fileUrl ? (
-                    <div>
-                        test-1
-                    </div>
-                ):
-                (
-                   <div className='flex justify-center items-center flex-col p-7 h-60 lg:h-[612px];'>
-                        <img src="/icons/file-upload.svg"
-                         alt="upload image"
-                         width={96}
-                         height={77} />
-                         <h3 className='nase-medium text-slate-400'>Drag photo here...</h3>
-                         <p className='small-regular mb-6 text-slate-400'>SVG, PNG, JPG</p>
-                         <Button className='bg-slate-500 hover:bg-slate-400'>
-                            Select from computer
-                         </Button>
-                   </div> 
-                )
+                    <>
+                        <div className='flex flex-1 justify-center w-full p-5 lg:p-10'>
+                            <img
+                                src={fileUrl}
+                                alt='image'
+                                className='h-80 lg:h-[480px] w-full rounded-[24px] object-cover object-top'
+                            />
+                        </div>
+                        <p className='mb-5  text-slate-400'>Click or drag photo to replace</p>
+                    </>
+                ) :
+                    (
+                        <div className='flex justify-center items-center flex-col p-7 h-60 lg:h-[612px];'>
+                            <img src="/icons/file-upload.svg"
+                                alt="upload image"
+                                width={96}
+                                height={77} />
+                            <h3 className='nase-medium text-slate-400'>Drag photo here...</h3>
+                            <p className='small-regular mb-6 text-slate-400'>SVG, PNG, JPG</p>
+                            <Button className='bg-slate-500 hover:bg-slate-400'>
+                                Select from computer
+                            </Button>
+                        </div>
+                    )
             }
         </div>
     )
