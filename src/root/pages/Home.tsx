ac8c1197587115fe539fbf1f82d6e2a56e16fbb5
DaniIliev <danieljivkov04@abv.bg>
2024-01-20 19:34:27 +0200

    create home page

--- src/root/pages/Home.tsx
+++ src/root/pages/Home.tsx
@@ -1,8 +1,31 @@
+import { Loader } from "@/components/shared/loader"
+import { useGetRecentPosts } from "@/lib/react-query/queryAndMutations"
+import { Models } from "appwrite"
 
 
 const Home = () => {
+  const { data: posts, isPending: isPostLoading, isError: isErrorPosts } = useGetRecentPosts()
+  const isLoading = true
+  console.log(posts)
   return (
-    <div>Home</div>
+    <div className="flex flex-1">
+      <div className='home-container'>
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full text-white">Home Feed</h2>
+          {isLoading && !posts ? (
+            <Loader />
+          ) : (
+            <ul className='flex flex-col flex-1 gap-4 w-full'>
+              
+                {posts?.documents.map((post: Models.Document) => (
+                  <li className="text-white">{post.caption}</li>
+                ))}
+            </ul>
+          )}
+        </div>
+
+      </div>
+    </div>
   )
 }
 
