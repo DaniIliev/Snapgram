14c130f65a683e0dfa0c1ebcffa8083bf3612865
DaniIliev <danieljivkov04@abv.bg>
2024-01-20 19:00:46 +0200

    creating post functionality

--- src/types/index.ts
+++ src/types/index.ts
@@ -19,3 +19,11 @@ export type INewUser = {
     route: string;
     label: string;
   };
+
+  export type INewPost = {
+    userId: string;
+    caption: string;
+    file: File[];
+    location?: string;
+    tags?: string;
+  };
