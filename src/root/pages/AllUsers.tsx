95b02f402237bf2ba8e9e7ca48090cfa2b289c2e
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2024-02-17 13:39:09 +0200

    listing of all users

--- src/root/pages/AllUsers.tsx
+++ src/root/pages/AllUsers.tsx
@@ -1,17 +1,26 @@
-import { useGetRecentPosts, useGetRecentUsers } from '@/lib/react-query/queryAndMutations'
-import React from 'react'
+import UserCard from "@/components/shared/UserCard";
+import {
+  useGetRecentPosts,
+  useGetRecentUsers,
+} from "@/lib/react-query/queryAndMutations";
+import { Models } from "appwrite";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const AllUsers = () => {
+  const {
+    data: users,
+    isPending: isUserLoading,
+    isError: isErrorPosts,
+  } = useGetRecentUsers();
 
-  const { data: users, isPending: isUserLoading, isError: isErrorPosts } = useGetRecentUsers()
-
-  console.log(users)
   return (
-    <div className='flex flex-col w-full'>
-
+    <div className="flex flex-col w-full gap-6">
+      {users?.documents.map((user: Models.Document) => (
+        <UserCard user={user}/> 
+      ))}
     </div>
-  )
-}
+  );
+};
 
-
-export default AllUsers
+export default AllUsers;
